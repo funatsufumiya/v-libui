53115e7a1f0de1f07800cd2b6577f913d7e5a521
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-04-06 10:19:48 +0900

    code cleanup

--- build.ps1
+++ build.ps1
@@ -4,15 +4,6 @@ Set-Location $PSScriptRoot
 
 Try { mkdir bin -ErrorAction SilentlyContinue > $null } Catch {}
 
-# @REM set "tcc=%CD%/tcc/tcc.exe"
-
-# @REM %tcc% -llibui -L%CD% -o "%CD%/bin/calender.exe" "%CD%/examples/calender/main.c" 
-# @REM %tcc% -llibui -L%CD% -o "%CD%/bin/widgets.exe" "%CD%/examples/widgets/main.c" 
-# @REM %tcc% -llibui -L%CD% -o "%CD%/bin/graph.exe" "%CD%/examples/graph/main.c" 
-# @REM %tcc% -llibui -L%CD% -o "%CD%/bin/label.exe" "%CD%/examples/label/main.c" 
-# @REM %tcc% -llibui -L%CD% -o "%CD%/bin/timer.exe" "%CD%/examples/timer/main.c" 
-# @REM %tcc% -llibui -L%CD% -o "%CD%/bin/minimal.exe" "%CD%/examples/minimal/main.c" 
-
 v "examples/minimal/main.v" -o "bin/minimal.exe"
 v -enable-globals "examples/timer/main.v" -o "bin/timer.exe"
 v -enable-globals "examples/calendar/main.v" -o "bin/calendar.exe"
--- build.sh
+++ build.sh
@@ -4,14 +4,6 @@ cd $(dirname $0)
 
 mkdir bin 1>/dev/null 2>/dev/null
 
-# CC=clang
-# $CC -lui -I. -L bin -o "bin/calender" "examples/calender/main.c" 
-# $CC -lui -I. -L bin -o "bin/widgets" "examples/widgets/main.c" 
-# $CC -lui -I. -L bin -o "bin/graph" "examples/graph/main.c" 
-# $CC -lui -I. -L bin -o "bin/label" "examples/label/main.c" 
-# $CC -lui -I. -L bin -o "bin/timer" "examples/timer/main.c" 
-# $CC -lui -I. -L bin -o "bin/minimal" "examples/minimal/main.c" 
-
 v examples/minimal/main.v -o bin/minimal
 v -enable-globals examples/timer/main.v -o bin/timer
 v -enable-globals examples/calendar/main.v -o bin/calendar
